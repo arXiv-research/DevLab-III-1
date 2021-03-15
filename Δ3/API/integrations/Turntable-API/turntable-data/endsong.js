d960a10b45ef2842083019ceb4b5163f6148f906
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-03-15 11:11:25 -0400

    Add files via upload

---
+++ Δ3/API/integrations/Turntable-API/turntable-data/endsong.js
@@ -0,0 +1,43 @@
+ {
+   "command": "endsong",
+   "room": {
+      "name": "@alain_gilbert", // Name of room.
+      "created": 1315459578.63, // Encoded creation date.
+      "shortcut": "alaingilbert", // Turntable link extension.
+      "name_lower": "@alain_gilbert", // Unknown
+      "roomid": "4e6851fa14169c2255108b8c", // Room ID
+      "metadata": {
+         "dj_full": false, // Is stage filled true or false.
+         "djs": ["4deadb0f4fe7d013dc0555f1"], // User IDs of current DJs.
+         "upvotes": 0, // Current amount of awesomes.
+         "privacy": "public", // Room privacy.
+         "max_djs": 5, // Number of DJ spots in room.
+         "downvotes": 0, // Current amount of lames.
+         "random": 0.400335315393, // Random generated number.
+         "userid": "4deadb0f4fe7d013dc0555f1", // User ID
+         "listeners": 2, // Current amount of listeners.
+         "djcount": 1, // Current amount of DJs.
+         "max_size": 200, // Max amount of people allowed in room.
+         "djthreshold": 0, // Points required to DJ.
+         "moderator_id": ["4deadb0f4fe7d013dc0555f1"], // User IDs of all room moderators of this room.
+         "current_song": {
+            "djid": "4deadb0f4fe7d013dc0555f1",
+            "_id": "4ddd08ad845daf4c250035cc",
+            "djname": "@alain_gilbert",
+            "starttime": 1327229746.2,
+            "metadata": {
+               "album": "The Fame",
+               "artist": "Lady GaGa",
+               "coverart": "http://images.mndigital.com/albums/018/334/899/m.jpeg",
+               "song": "Poker Face",
+               "length": 237,
+               "mnid": "18334907",
+               "genre": "Pop"
+            }
+         },
+         "current_dj": "4deadb0f4fe7d013dc0555f1",
+         "votelog": [['50661f0390001b4', 'up' ],[ '4df0cbe44fe7d2', 'up']]
+      }
+   },
+   "success": true
+}

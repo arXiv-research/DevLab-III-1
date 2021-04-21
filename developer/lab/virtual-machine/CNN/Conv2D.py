31a3a2e76a389410533779938e3ac55965cc9259
J.MITCHΞLL <71776129+MitchellTesla@users.noreply.github.com>
2021-04-20 20:54:24 -0400

    Update Conv2D.py

--- developer/lab/virtual-machine/CNN/Conv2D.py
+++ developer/lab/virtual-machine/CNN/Conv2D.py
@@ -9,29 +9,29 @@ from tensorflow.examples tutorials.mist import input data
 from PIL import Image
 from tensor flow.keras.preprocessing. image import ImageDataBenerator
 
-from tensorflow.keras.layers import
+from tensorflow.keras.layers import *
 
 #load data
 
 img_width = 150
 img_height = 150
-train data dir = 'data/train:
-valid_data_dir = 'data/validation
+train data dir = 'data/train'
+valid_data_dir = 'data/validation'
 
 datagen = ImageDataGenerator (rescale = 1./255)
 
-train_generator = datagen flow_from_directory
-directory=train_data_dir,
-target_size=(img_width, img_height),
-classes=['dogs, cats'],
-class_mode='binary
-batch size=16)
+train_generator = datagen.flow_from_directory(
+                directory=train_data_dir,
+                target_size=(img_width, img_height),
+                classes=['dogs','cats'],
+                class_mode='binary'
+                batch size=16)
 
-validation generatore datagen, flow from directory(directory valid data dir,
-target size=(img width, img height),
-classesal'dogs', 'eats'l.
-class mode='binary
-bateh size=32)
+validation_generator = datagen.flow_from_directory(directory=valid_data_dir,
+                target size=(img_width, img_height),
+                classes=['dogs', 'cats'],
+                class_mode='binary'
+                bateh_size=32)
 
 # build model
 
@@ -55,24 +55,24 @@ model.add(Dropout(0.5))
 model.add(Dense(2, activation='softmax'))
           
 model.compile(optimizer=tf.train. AdamOptimizer().
-loss='sparse_categorical_crossentropy',
-metrics=['accuracy'])
+                              loss='sparse_categorical_crossentropy',
+                              metrics=['accuracy'])
           
-print (model.summary()
+print (model.summary())
        
 #train model
        
-print('starting training....)
+print('starting training....')
 history = model.fit_generator (generator=train_generator,
-steps_per_epoch=2048 // 16, epochs=20,
-validation_data=validation_generator, validation_steps=832//16)
+        steps_per_epoch=2048 // 16, epochs=20,
+        validation_data=validation_generator, validation_steps=832//16)
       
 print('training finished!!)
       
       
 # save coefficients
       
-model save("CatsVersusDogs.trained")
+model.save("CatsVersusDogs.trained")
       
 # Get training and test loss histories
 training_loss = history history['loss']
@@ -82,7 +82,7 @@ epoch_count = range(1, len(training loss) + 1)
       
 # Visualize loss history
 plt. figure(0)
-plt.plot(epoch_count, training loss, '--')
+plt.plot(epoch_count, training loss, 'r--')
 plt.plot(epoch_count, accuracy, 'b--')
 pit. legend( "Training Loss', 'Accuracy'])
 plt.xlabel('Epoch)

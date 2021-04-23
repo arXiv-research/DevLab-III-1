853ca6fc366fdb4d049bda60543fa081eb05db7c
J.MITCHΞLL <71776129+MitchellTesla@users.noreply.github.com>
2021-04-23 00:02:16 -0400

    Updating _Thread _ModuleBase

---
+++ developer/lab/virtual-machine/OpenKAI/Application/Autopilot/_AP_avoid.h
@@ -0,0 +1,42 @@
+#ifndef OpenKAI_src_Autopilot_AP__AP_avoid_H_
+#define OpenKAI_src_Autopilot_AP__AP_avoid_H_
+
+#include "../../../Detector/_DetectorBase.h"
+#include "../ArduPilot/_AP_base.h"
+
+#ifdef USE_OPENCV
+
+namespace kai
+{
+
+class _AP_avoid: public _StateBase
+{
+public:
+	_AP_avoid();
+	~_AP_avoid();
+
+	bool init(void* pKiss);
+	bool start(void);
+	int	 check(void);
+	void update(void);
+	void draw(void);
+
+private:
+	void updateTarget(void);
+	static void* getUpdate(void* This)
+	{
+		((_AP_avoid*) This)->update();
+		return NULL;
+	}
+
+private:
+	_AP_base* m_pAP;
+	_DetectorBase* m_pDet;
+	_Mavlink* m_pMavlink;
+
+	_Object m_obs;
+};
+
+}
+#endif
+#endif

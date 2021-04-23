87f490653a166c9547c19cdd3dd0c47bd8365648
J.MITCHΞLL <71776129+MitchellTesla@users.noreply.github.com>
2021-04-22 23:56:28 -0400

    Updated

---
+++ developer/lab/virtual-machine/OpenKAI/Application/Autopilot/_AP_gcs.h
@@ -0,0 +1,43 @@
+#ifndef OpenKAI_src_Application_Autopilot_ArduPilot__AP_gcs_H_
+#define OpenKAI_src_Application_Autopilot_ArduPilot__AP_gcs_H_
+
+#include "_AP_base.h"
+#include "../../../GCS/_GCSbase.h"
+
+namespace kai
+{
+
+class _AP_gcs: public _GCSbase
+{
+public:
+	_AP_gcs();
+	~_AP_gcs();
+
+	virtual bool init(void* pKiss);
+	virtual bool start(void);
+	virtual int check(void);
+	virtual void update(void);
+	virtual void draw(void);
+    
+    void landingReady(bool bReady);
+    void takeoffReady(bool bReady);
+    
+protected:
+    virtual void updateGCS (void);
+	static void* getUpdate(void* This)
+	{
+		(( _AP_gcs*) This)->update();
+		return NULL;
+	}
+
+public:
+    _AP_base* m_pAP;
+
+    bool m_bAutoArm;
+    float m_altAirborne;
+    float m_altLand;
+    int m_dLanded;
+};
+
+}
+#endif
